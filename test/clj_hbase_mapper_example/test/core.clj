79ef3712f26d4d32c912708db488df51a5e79191
Zhemin Lin <lin.zhemin@gmail.com>
2013-03-08 17:52:55 +0800

    Correct :use

---
+++ test/clj_hbase_mapper_example/test/core.clj
@@ -0,0 +1,6 @@
+(ns clj-hbase-mapper-example.test.core
+  (:use [clj-hbase-mapper-example.local])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
