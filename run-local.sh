864ff7cd45c949f33a99c1248ac180de47a1e6d7
Zhemin Lin <lin.zhemin@gmail.com>
2013-03-08 17:54:38 +0800

    Remove ./lib/* from classpath

--- run-local.sh
+++ run-local.sh
@@ -5,5 +5,5 @@ if [ $# -lt 3 ]; then
 	echo "Ex:    cat ../2 | run-local.sh value default.htable cf:cq"
 	exit 1
 fi
-CP="clj-hbase-mapper-example-0.0.1-standalone.jar:./lib/*"
+CP="clj-hbase-mapper-example-0.0.1-standalone.jar"
 java -cp "/etc/hadoop/conf:/usr/lib/hadoop/*:/usr/lib/hadoop/lib/*:/etc/hbase/conf:/usr/lib/hbase/hbase.jar:/usr/lib/hbase/lib/*:$CP" -server -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -Xms512M -Xmx1024M  clj_hbase_mapper_example.local $*
